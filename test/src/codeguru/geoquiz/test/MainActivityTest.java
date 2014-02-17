3da51ffdba9b076346254fade2da50d52a28aae6
codeguru <codeguru@users.sourceforge.net>
2014-02-16 17:25:12 -0700

    Stub out `MainActivityTest`

---
+++ test/src/codeguru/geoquiz/test/MainActivityTest.java
@@ -0,0 +1,16 @@
+package codeguru.geoquiz.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import codeguru.geoquiz.MainActivity;
+
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+    
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+    
+    @Override
+    protected void setUp() {
+    }
+
+}
