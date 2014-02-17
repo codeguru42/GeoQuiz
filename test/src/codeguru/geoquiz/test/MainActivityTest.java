bbd5c0538d2572fe395e97127d7535a02d59b323
codeguru <codeguru@users.sourceforge.net>
2014-02-16 20:14:51 -0700

    Simplify `testPreconditions()`

--- test/src/codeguru/geoquiz/test/MainActivityTest.java
+++ test/src/codeguru/geoquiz/test/MainActivityTest.java
@@ -3,31 +3,29 @@ package codeguru.geoquiz.test;
 import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
 import codeguru.geoquiz.MainActivity;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.File;
 import junit.framework.Assert;
 
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
-    
+
     private static final String DB_NAME = "geoquiz.sqlite";
-    
+
     private Activity mActivity;
-    
+
     public MainActivityTest() {
         super(MainActivity.class);
     }
-    
+
     @Override
     protected void setUp() {
         mActivity = getActivity();
     }
-    
-    public void testPreconditions() throws IOException {
+
+    public void testPreconditions() {
         Assert.assertNotNull(mActivity);
-        
-        InputStream in = mActivity.openFileInput(DB_NAME);
-        Assert.assertNotNull(in);
-        in.close();
+
+        File dbFile = new File(mActivity.getFilesDir(), DB_NAME);
+        Assert.assertTrue(dbFile.exists());
     }
 
 }
