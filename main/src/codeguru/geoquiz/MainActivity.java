78aa10850d346f4dc1871193a7131a8e4c4be294
codeguru <codeguru@users.sourceforge.net>
2014-02-16 19:44:20 -0700

    Implement `copyDatabaseAsset()`

--- main/src/codeguru/geoquiz/MainActivity.java
+++ main/src/codeguru/geoquiz/MainActivity.java
@@ -2,14 +2,28 @@ package codeguru.geoquiz;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class MainActivity extends ActionBarActivity {
+
+    private static final String TAG = MainActivity.class.getName();
+
+    private static final String DB_NAME = "geoquiz.sqlite";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            copyDatabaseAsset();
+        } catch (IOException e) {
+            Log.e(TAG, e.getMessage(), e);
+        }
     }
 
     @Override
@@ -17,6 +31,34 @@ public class MainActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    private void copyDatabaseAsset() throws IOException {
+        InputStream in = null;
+        OutputStream out = null;
+
+        try {
+            in = getAssets().open(DB_NAME);
+            out = openFileOutput(DB_NAME, MODE_PRIVATE);
+
+            byte[] buffer = new byte[1024];
+            int read;
+
+            // Copy from input stream to output stream
+            while ((read = in.read(buffer)) != -1) {
+                out.write(buffer, 0, read);
+            }
+        } catch (IOException e) {
+            Log.e(TAG, e.getMessage(), e);
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+
+            if (out != null) {
+                out.close();
+            }
+        }
     }
 
 }
