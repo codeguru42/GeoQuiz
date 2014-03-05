7bd6488436df3ece709c519accc78d9b57628f76
codeguru <codeguru@users.sourceforge.net>
2014-03-04 17:29:35 -0700

    Create `Country` and `LookAt` POJOs

---
+++ main/src/codeguru/geoquiz/data/Country.java
@@ -0,0 +1,14 @@
+package codeguru.geoquiz.data;
+
+import com.google.android.gms.maps.model.LatLng;
+import java.util.List;
+
+public class Country {
+
+    public String name;
+
+    public LookAt lookAt;
+
+    public List<LatLng> border;
+
+}
