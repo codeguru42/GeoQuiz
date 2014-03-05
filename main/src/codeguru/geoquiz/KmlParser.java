786313a4aae722778d927214a812ea7d98192648
codeguru <codeguru@users.sourceforge.net>
2014-03-04 17:56:37 -0700

    Implement `parse()`

--- main/src/codeguru/geoquiz/KmlParser.java
+++ main/src/codeguru/geoquiz/KmlParser.java
@@ -1,5 +1,30 @@
 package codeguru.geoquiz;
 
+import android.util.Xml;
+import codeguru.geoquiz.data.Country;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
 public class KmlParser {
+
+    public List<Country> parse(InputStream in) throws IOException,
+            XmlPullParserException {
+        try {
+            XmlPullParser parser = Xml.newPullParser();
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            parser.setInput(in, null);
+            parser.nextTag();
+            return parseCountries(parser);
+        } finally {
+            in.close();
+        }
+    }
+
+    private List<Country> parseCountries(XmlPullParser parser) {
+        return null;
+    }
 
 }
